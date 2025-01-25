7a68afb5c887762de1c2eec697c14df35dac7643
damaske <damaske.vzk@gmail.com>
2025-01-25 17:26:03 +0000

    fix

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,10 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result_list = []
     for i in range(list_length):
         try:
-            if i < len(my_list_1) and i < len(my_list_2):
             element_div = my_list_1[i] / my_list_2[i]
-        else:
-            raise IndexError
         except ZeroDivisionError:
             print("division be 0")
             element_div = 0
