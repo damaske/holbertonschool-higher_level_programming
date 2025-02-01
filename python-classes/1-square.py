dde8e7ade446c71a1a3cc7638f58abdc1015efd6
damaske <damaske.vzk@gmail.com>
2025-02-01 13:46:36 +0000

    1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """This class defines a square with a specified size."""
+    def __init__(self, size):
+        self.__size = size
