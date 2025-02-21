052325c2efa540365ee40c11d149dc73ebc80b8f
damaske <damaske.vzk@gmail.com>
2025-02-21 22:42:19 +0000

    Write a class Square that defines a square with position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,4 +45,3 @@ class Square:
                 not all(isinstance(i, int) and i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
