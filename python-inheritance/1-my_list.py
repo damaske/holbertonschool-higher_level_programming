1ce3f31e1569f9a89460998e99a3990d15cf7d4a
damaske <damaske.vzk@gmail.com>
2025-03-02 15:44:20 +0000

    adding doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""A subclass of list that includes a method
+to print the list in sorted order."""
+
 
 class MyList(list):
+"""Prints the list elements in ascending sorted order.
+This method does not modify the original list; it only prints a sorted copy."""
+
     def print_sorted(self):
-        return sorted(self
+        return sorted(self)
