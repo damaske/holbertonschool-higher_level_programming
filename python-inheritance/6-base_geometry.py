4faddc328996593a53674d8a0e002c47d04c5253
damaske <damaske.vzk@gmail.com>
2025-03-03 19:07:45 +0000

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This class contains class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """method area"""
+
+    def area(self):
+        """Exception area() is not implemented"""
+
+        raise Exception("area() is not implemented")
