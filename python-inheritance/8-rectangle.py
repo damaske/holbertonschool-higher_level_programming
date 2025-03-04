6ee5749002dfc3027938dc05c20cba304c788e89
damaske <damaske.vzk@gmail.com>
2025-03-04 21:13:53 +0000

    Instantiation with width and height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""This class contains class named BaseGeometry"""
+"""This class contains class named Rectangle(BaseGeometry):"""
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     """nstantiation with width and height"""
 
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """Exception area() is not implemented"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not type(value) is int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
