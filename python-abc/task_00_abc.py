48cb1760b03184bd1d3601d2f1f20da610302284
damaske <damaske.vzk@gmail.com>
2025-03-23 20:10:53 +0000

    task_00_abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from abc import ABC
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
