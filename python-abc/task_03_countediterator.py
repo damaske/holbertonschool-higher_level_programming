0c47ca4bf5e672b05b3adf23ce7487f5887d3b6b
damaske <damaske.vzk@gmail.com>
2025-03-24 23:58:32 +0000

    CountedIterator - Keeping Track of Iteration

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,11 +1,11 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 
 class CountedIterator:
     def __init__(self, iterable):
         self.iterator = iter(iterable)
         self.count = 0
 
-    def get_count:
+    def get_count(self):
         return self.count
 
     def __next__(self):
