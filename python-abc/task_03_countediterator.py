fe162999efc843abb668322a328f54571a754316
damaske <damaske.vzk@gmail.com>
2025-03-24 20:21:40 +0000

    raw

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+
+class CountedIterator:
+    def __init__(self, iterable, count):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def get_count:
+        return self.count
+
+    def __next__(self):
+        pass
