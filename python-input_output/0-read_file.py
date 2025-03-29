59a8dff26313cf9fac8cb0dd8317a90ae87f512b
damaske <damaske.vzk@gmail.com>
2025-03-30 03:31:21 +0400

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module contains a function that reads a UTF-8 file and prints it."""
 
+
 def read_file(filename=""):
-    
+  
     """Function that reads a text file (UTF8) and prints it to stdout."""
-   
+
     with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read(), end="")
