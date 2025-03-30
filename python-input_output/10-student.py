6c588f8a62043bdf1c4e79fe9627e5f4c53e4425
damaske <damaske.vzk@gmail.com>
2025-03-30 21:47:31 +0400

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Class Student that defines a student."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        My_d = {}
+        for key in attrs:
+            if key in self.__dict__:
+                My_d[key] = self.__dict__[key]
+                return My_d
