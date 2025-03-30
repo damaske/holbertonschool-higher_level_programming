4189a068272515e601304fd2a9f361b5df790257
damaske <damaske.vzk@gmail.com>
2025-03-30 18:08:44 +0400

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,7 +7,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """Retrieves a dictionary representation of a Student"""
         return self.__dict__
