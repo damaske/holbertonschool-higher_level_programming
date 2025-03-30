4683681bc87563958f624cca064d01f6a7a1e3ad
damaske <damaske.vzk@gmail.com>
2025-03-30 04:09:17 +0400

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains write_file() function"""
+
+
+def write_file(filename="", text=""):
+
+    """Function that writes a string to a text file (UTF8) and returns the number of characters written."""
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
