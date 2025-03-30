0dcddff3ac6d44fbb376f0a89e6a9c394aa880c2
damaske <damaske.vzk@gmail.com>
2025-03-30 05:57:53 +0400

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module to_json_string() file"""
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """Function that returns the JSON representation 
+    of an object (string)."""
+
+    return json.dumps(my_obj)
