1623f1f6f7e006668c61c044f1d9b6f8ca55dda0
damaske <damaske.vzk@gmail.com>
2025-03-30 04:11:47 +0400

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     """Function that writes a string to a text file (UTF8) and returns the number of characters written."""
 
     with open(filename, "w", encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
