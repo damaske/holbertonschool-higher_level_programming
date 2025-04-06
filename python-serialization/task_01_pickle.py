ca635052dabae42f02593992afa75790907682b3
damaske <damaske.vzk@gmail.com>
2025-04-07 00:05:53 +0400

    Pickling Custom Classes

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -2,17 +2,18 @@
 import pickle
 import os
 
+
 class CustomObject:
     def __init__(self, name: str, age: int, is_student: bool):
         self.name = name
         self.age = age
         self.is_student =is_student
-        
+
     def display(self):
         """Display the object's attributes."""
         for key in self.__dict__.keys():
             print(f"{key.capitalize()}: {self.__dict__[key]}")
-    
+
     def serialize(self, filename: str):
         """Serialize the object and save it to a file."""
         try:
@@ -20,7 +21,7 @@ class CustomObject:
                 pickle.dump(self, file)
         except Exception:
             return None
-    
+
     @classmethod 
     def deserialize(cls, filename: str):
         """Load and return an instance of CustomObject from a file."""
