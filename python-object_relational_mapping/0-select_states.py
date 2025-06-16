c3b28fca0b9ec823574f8c60e5e524c489598625
damaske <damaske.vzk@gmail.com>
2025-06-16 19:11:35 +0400

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
         host="localhost",
         port=3306,
         user=argv[1],
-        password=argv[2],
-        database=argv[3]
+        passwd=argv[2],
+        db=argv[3]
     )
     cursor = db.cursor()
 
