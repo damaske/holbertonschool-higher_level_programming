4d4d0c9943ba0f7cb5d4839b4a567ec98b8f3143
root <damaske.vzk@gmail.com>
2025-06-27 12:57:26 +0000

    permission

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     session = Session()
     
     for state in session.query(State).order_by(State.id):
-        print("{}:{}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     
     if session:
         session.close()
